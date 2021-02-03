cbc1f9992214bac41a0fbbe360596417f8b428fa
Wagner Xu <wagnerhsu@hotmail.com>
2021-02-03 17:44:20 +0800

    Test FullCalendar

---
+++ AdminLTE.MVC/wwwroot/js/index.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    //var calendarEl = document.getElementById('calendar');
+    //var calendar = new FullCalendar.Calendar(calendarEl, {
+    //    initialView: 'dayGridMonth'
+    //});
+    //calendar.render();
+});
